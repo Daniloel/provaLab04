4aed0c6316f2554cd81a1c057aff8f9788ac3666
Daniloel <zoltancao@gmail.com>
2023-04-11 00:47:17 -0300

    teste

---
+++ src/main/java/DLG/SJC/Lab04_Springboot/Lab04SpringbootApplication.java
@@ -0,0 +1,13 @@
+package DLG.SJC.Lab04_Springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab04SpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab04SpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/DLG/SJC/Lab04_Springboot/Lab04SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package DLG.SJC.Lab04_Springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab04SpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
